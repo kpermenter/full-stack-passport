9113ff3a3f40fb738817a345128d20fa220c2498
Joaquin Vargas <joaquin.vargas90@gmail.com>
2020-01-11 12:35:47 -0600

    facebook

--- app.js
+++ app.js
@@ -49,4 +49,24 @@ models.sequelize.sync().then(function(){
   });
 });
 
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '585359305586524',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v5.0'
+    });
+      
+    FB.AppEvents.logPageView();   
+      
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
 module.exports = app;
